0ddaf3c430abeb975f1a72045c6e9df83721ae6d
gololobov <gololobov.serhiy@gmail.com>
2024-04-16 20:39:12 +0300

    16.04.2024

---
+++ Lesson 1.py
@@ -0,0 +1,40 @@
+print("Hello Python") # це друкує текст
+#print(2024, "mama")
+'''print("mama", end=' ')
+print(2024)
+print(2024, True, "papa", 2.36, sep=' * ')
+print('\tmama\t\tpapa')'''
+
+'''print('кожен\n\tмисливець')
+
+print("mama\bpapa")
+print("mama\rpapa")
+print('mama')
+print("m'ята")
+print('m\'ята')
+
+# a..z A..Z 0..9 _
+
+a = 3.14
+print(type(3.25))
+print(a, 2022)'''
+
+# int
+# float
+# str
+# bool
+
+
+print("kj wej owie jwoief")
+a = int(input("Введить значення сторони а: "))
+b = int(input("Введить значення сторони b: "))
+s = a * b
+print('Площа прямокутника зі сторонами а =', a, ', b =', b , 'дорівнює', s)
+
+
+
+
+
+
+
+
